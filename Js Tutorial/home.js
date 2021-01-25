a19838e451a804e7d57369c8686adada4fc7e60f
Muscalu <Darius>
2021-01-25 23:02:28 +0200

    Little Project Added

---
+++ Js Tutorial/home.js
@@ -0,0 +1,7 @@
+var username=prompt("What is your name?","your name");
+
+if(username==="Darius") {
+    document.write("<h1>Hello " + username + " You won an Nissan GTR</h1>");
+} else {
+    document.write("<h1>Hello " + username + "!");
+}
